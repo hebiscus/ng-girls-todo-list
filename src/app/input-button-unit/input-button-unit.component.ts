575f5027382011fe4909d01c73b3fc89e56da729
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-11-05 12:24:20 +0100

    add basic input-button

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'todo-input-button-unit',
@@ -10,6 +10,16 @@ import { Component } from '@angular/core';
   `,
   styleUrls: ['./input-button-unit.component.scss']
 })
-export class InputButtonUnitComponent {
+export class InputButtonUnitComponent implements OnInit {
+  ngOnInit(): void {
+  }
   title = 'Hello World';
+
+  constructor() { 
+    this.changeTitle('My First Angular App');
+  }
+
+  changeTitle(newTitle: string) {
+    this.title = newTitle;
+  }
 }
