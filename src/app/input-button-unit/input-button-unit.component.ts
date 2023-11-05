9cfb3b54048ccc2fc82484365259a73f4d3db4cd
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-11-05 12:51:48 +0100

    add input reference

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -3,14 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'todo-input-button-unit',
   template: `
-    <p>
-      input-button-unit works!
-      The title is: {{ title }}
-    </p>
-
-    <input  [value]="title"
+    <input  #inputElementRef
+            [value]="title"
             (keyup)="changeTitle(getInputValue($event))">
-    <button (click)="changeTitle('Button Clicked!')">Save</button>
+    <button (click)="changeTitle(inputElementRef.value)">Save</button>
   `,
   styleUrls: ['./input-button-unit.component.scss']
 })
